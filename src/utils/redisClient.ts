0268baf0176a0827db546d8fc9d148f80266b3c0
Muhammed-Bilal777 <mdbilaal777@gmail.com>
2024-12-19 00:56:53 +0530

    fixed cache issues

--- src/utils/redisClient.ts
+++ src/utils/redisClient.ts
@@ -1,104 +1,13 @@
-// import Redis from "ioredis";
-
-// const redisClient = new Redis({
-//   host: "redis",
-//   port: 6379,
-//   db: 0,
-// });
-
-// export const generateUniqueCacheKey = (
-//   queryParams: Record<string, string | undefined>
-// ) => {
-//   const keys = Object.keys(queryParams)
-//     .filter((key) => queryParams[key])
-//     .sort();
-//   const queryString = keys.map((key) => `${key}:${queryParams[key]}`).join("|");
-//   return `files:cache:${queryString}`;
-// };
-
-// export const cacheData = async (key: string, data: any): Promise<void> => {
-//   try {
-//     await redisClient.set(key, JSON.stringify(data), "EX", 3600);
-//     console.log(`Cache set for key: ${key}`);
-//   } catch (err) {
-//     console.error(`Error setting cache for key: ${key}`, err);
-//   }
-// };
-
-// export const getCachedData = async (key: string): Promise<any | null> => {
-//   try {
-//     const cached = await redisClient.get(key);
-//     if (cached) {
-//       return JSON.parse(cached);
-//     }
-//     return null;
-//   } catch (err) {
-//     console.error(`Error fetching cache for key: ${key}`, err);
-//     return null;
-//   }
-// };
-
-// export const resetCache = async (key?: string, data?: any): Promise<void> => {
-//   try {
-//     if (key && data) {
-//       const cachedData = await redisClient.get("files:cache:");
-//       if (cachedData) {
-//         const parsedData = JSON.parse(cachedData);
-
-//         const filteredCache = parsedData.filter(
-//           (item: any) => item.fileName !== key
-//         );
-
-//         filteredCache.push(data);
-
-//         await redisClient.set(
-//           "files:cache:",
-//           JSON.stringify(filteredCache),
-//           "EX",
-//           3600
-//         );
-//         console.log("Cache updated after file upload");
-//       } else {
-//         await redisClient.set(
-//           "files:cache:",
-//           JSON.stringify([data]),
-//           "EX",
-//           3600
-//         );
-//         console.log("Cache set with new data for all files");
-//       }
-//     } else {
-//       await redisClient.del("files:cache:");
-//       console.log("Cache invalidated for all files");
-//     }
-//   } catch (err) {
-//     console.error("Error resetting cache:", err);
-//   }
-// };
-
-// export const invalidateCache = async (key: string): Promise<void> => {
-//   try {
-//     await redisClient.del(key);
-//     console.log(`Cache invalidated for key: ${key}`);
-//   } catch (err) {
-//     console.error(`Error invalidating cache for key: ${key}`, err);
-//   }
-// };
-
-// src/utils/RedisClient.ts
-
 import { IRedisClient } from "./interfaces/IRedisClient";
 import Redis from "ioredis";
 
-// Initialize the Redis client
 const redisClient = new Redis({
-  host: "redis",
-  port: 6379,
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
   db: 0,
 });
 
 export class RedisClient implements IRedisClient {
-  // Generate a unique cache key based on query parameters
   generateUniqueCacheKey(
     queryParams: Record<string, string | undefined>
   ): string {
